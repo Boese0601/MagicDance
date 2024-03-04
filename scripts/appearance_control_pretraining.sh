c55b108601270199c078155d39edf7da0a03c0d8
Di Chang <dichang@usc.edu>
2024-03-03 20:25:49 -0800

    Update appearance_control_pretraining.sh

--- scripts/appearance_control_pretraining.sh
+++ scripts/appearance_control_pretraining.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0 torchrun --master_port 10000 train_tiktok.py \
 --model_config model_lib/ControlNet/models/cldm_v15_reference_only.yaml \
 --init_path ./pretrained_weights/control_sd15_ini.ckpt \
 --output_dir ./tiktok_train_log/appearance_control_pretraining \
---train_batch_size 1 \
+--train_batch_size 32 \
 --num_workers 1 \
 --img_bin_limit 15 \
 --use_fp16 \
